bd730b3d454d85a3f80499d0ca98aadd755bb4fe
Caleb Ghirmai <calebyg15@gmail.com>
2020-09-14 18:31:49 -0700

    commit

---
+++ TermBank/src/main/java/termbank/TermBankApp.java
@@ -0,0 +1,64 @@
+/*
+ * Programmer: Caleb Ghirmai
+ * 
+ * File: TermBankApp.java
+ * 
+ * This file runs the JavaFX application
+ */
+package termbank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TermBankApp extends Application {
+
+	private Stage primaryStage;
+	private static Scene primaryScene;
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		this.primaryStage = stage;
+		this.primaryStage.setTitle("TermBank 1.0");
+		
+		primaryScene = new Scene(loadFXML("/fxml/BankSheetLayout"), 500, 500);
+
+		String stylesheet = TermBankApp.class.getResource("/stylesheets/stylesheet.css").toExternalForm();
+		primaryScene.getStylesheets().clear();
+		primaryScene.getStylesheets().add(stylesheet);
+			
+		
+		this.primaryStage.setScene(this.primaryScene);
+		this.primaryStage.setResizable(false);
+		this.primaryStage.show();
+	}
+
+	// This is for switching screens/ views
+	public static void setRoot(String fxml) throws IOException{
+		primaryScene.setRoot(loadFXML(fxml));
+	}
+
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(TermBankApp.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
+	
+	public Stage returnStage() {
+		return this.primaryStage;
+	}
+	
+	public Scene returnScene() {
+		return this.primaryScene;
+	}
+
+}
